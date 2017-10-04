17213cf85793407c583cd3b16fa540279875171e
George Spake <gpspake@gmail.com>
2017-10-04 12:16:31 -0500

    fix eslint space error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const App = () => (
   <div className="App">
     <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo"/>
+      <img src={logo} className="App-logo" alt="logo" />
       <h2>Welcome to React</h2>
     </div>
     <p className="App-intro">
